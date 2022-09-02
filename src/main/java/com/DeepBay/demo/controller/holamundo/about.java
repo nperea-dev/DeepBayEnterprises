532b7d57d3fd506bb0c1f272e72a0edb41c381f7
Ney Perea <nperea75@gmail.com>
2022-09-02 07:08:52 -0500

    se crea rama spring con proyecto basico /saludo /about

---
+++ src/main/java/com/DeepBay/demo/controller/holamundo/about.java
@@ -0,0 +1,25 @@
+package com.DeepBay.demo.controller.holamundo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/about")
+public class about {
+    @GetMapping
+    public String about() {
+        return "<html>\n" +
+                "<body>\n" +
+                "\n" +
+                "<h2 style=\"color:red;\">Workgroup Ciclo 3 Ds.</h2>\n" +
+                "\n" +
+                "<h3 style=\"color:blue;\">Andres, Allison, Cindy, Ney, Zaymon </h3>\n" +
+                "\n" +
+                "</body>\n" +
+                "</html>\n ";
+        }
+    }
+
