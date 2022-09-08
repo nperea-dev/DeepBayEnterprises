25b03cba14dfb477ff9a43197151ea19dc9ce11c
Ney Perea <nperea75@gmail.com>
2022-09-08 07:38:32 -0500

    se actuliza readme.md, se agregan las dependecias:springdata jpa y postgresjdbc se actualiza el application properties para acceder a la base de datos en supabase, solo se crea la tabla empleado

--- src/main/java/com/DeepBay/demo/Entities/Empleado.java
+++ src/main/java/com/DeepBay/demo/Entities/Empleado.java
@@ -1,11 +1,24 @@
-package com.example.demo.entidades;
+package com.DeepBay.demo.Entities;
 
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="empleado")
 public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name ="nombre_Empleado")
     private String NombreEmpleado;
+    @Column(name = "Correo_corp")
     private String CorreoEmpleado;
+    @Column(name = "Empresa")
     private String EmpresaEmpleado;
+    @Column(name = "Rol_empleado")
     private String RolEmpleado;
 
+    public Empleado(){}
     public Empleado(String nombreEmpleado, String correoEmpleado, String empresaEmpleado, String rolEmpleado) {
         this.NombreEmpleado = nombreEmpleado;
         this.CorreoEmpleado = correoEmpleado;
