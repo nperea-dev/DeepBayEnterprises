7f18f360e65244e4146adf535a898798dacda71d
ney roger <52115064+nperea75-gh@users.noreply.github.com>
2022-09-16 08:09:15 -0500

    Delete AutosDB directory

--- AutosDB/src/main/java/com/enterprise/AutosDB/repositories/EnterpriseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.enterprise.AutosDB.repositories;
-
-import com.enterprise.AutosDB.entities.Enterprise;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnterpriseRepository  extends JpaRepository<Enterprise, Long> {
-}
--- AutosDB/src/main/java/com/enterprise/AutosDB/repositories/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.enterprise.AutosDB.repositories;
-
-import com.enterprise.AutosDB.entities.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionRepository  extends JpaRepository<Transaction, Long> {
-}
