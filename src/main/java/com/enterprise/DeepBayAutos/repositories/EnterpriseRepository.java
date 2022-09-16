55400e3284bbc8f070c930e4d32e4a0ad7646a1d
Ney Perea <nperea75@gmail.com>
2022-09-15 23:16:17 -0500

    se borran archivos

--- src/main/java/com/enterprise/DeepBayAutos/repositories/EnterpriseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.enterprise.DeepBayAutos.repositories;
-
-import com.enterprise.DeepBayAutos.entities.Enterprise;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnterpriseRepository  extends JpaRepository<Enterprise, Long> {
-}
--- src/main/java/com/enterprise/DeepBayAutos/repositories/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.enterprise.DeepBayAutos.repositories;
-
-import com.enterprise.DeepBayAutos.entities.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionRepository  extends JpaRepository<Transaction, Long> {
-}
